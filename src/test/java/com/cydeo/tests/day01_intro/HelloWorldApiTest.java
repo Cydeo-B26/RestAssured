b008199a4e5d9ed4017eeef5ea138d1658b8dc4c
Murodil <murodil@cybertekschool.com>
2022-07-31 15:08:22 -0400

    HelloWorldApiTest

--- src/test/java/com/cydeo/tests/day01_intro/HelloWorldApiTest.java
+++ src/test/java/com/cydeo/tests/day01_intro/HelloWorldApiTest.java
@@ -9,13 +9,27 @@ import org.junit.jupiter.api.Test;
 public class HelloWorldApiTest {
     String url = "https://sandbox.api.service.nhs.uk/hello-world/hello/world";
 
+    /**
+     When user sends GET Request to :
+     https://sandbox.api.service.nhs.uk/hello-world/hello/world
+
+     Then status code should be 200
+     And "Hello World!" message should be in the response
+     */
     @DisplayName("Hello World GET request")
     @Test
     public void helloWorldGetRequestTest(){
         //send a get request and save response inside the Response object
         Response response = RestAssured.get(url);
 
+        //print response body in a formatted way
         response.prettyPrint();
+
+        //print status code
+        System.out.println("Status code = " + response.statusCode());
+        System.out.println("Status line = " + response.statusLine());
+
+
 
     }
 
