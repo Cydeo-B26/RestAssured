7f5abfd3a03783e463c02b5c18acd0bf6f55cb31
Murodil <murodil@cybertekschool.com>
2022-08-03 20:58:24 -0400

    path paramsnot found

--- src/test/java/com/cydeo/tests/day03_parameters/SpartanApiWithParamsTest.java
+++ src/test/java/com/cydeo/tests/day03_parameters/SpartanApiWithParamsTest.java
@@ -29,8 +29,8 @@ public class SpartanApiWithParamsTest {
                 .when().get(url+"/" + id); //id value is concatenated to url
 
        Response response = given().accept(ContentType.JSON)
-                .and().pathParam("id", id) //id value is passed using pathParam method
-                .when().get(url+"/{id}");
+                .and().pathParam("id", 5) //id value is passed using pathParam method
+                .when().get(url+"/{id}"); // /5
 
        response.prettyPrint();
         System.out.println("status code = " + response.statusCode());
@@ -45,7 +45,47 @@ public class SpartanApiWithParamsTest {
 
         assertTrue(response.asString().contains("Blythe"));
     }
+
+/**
+*         Given accept type is Json
+*         And Id parameter value is 500
+*         When user sends GET request to /api/spartans/{id}
+*         Then response status code should be 404
+*         And response content-type: application/json
+*         And "Not Found" message should be in response payload
+*/
+
+    @DisplayName("GET /api/spartans/{id} with missing id")
+    @Test
+    public void getSingleSpartanNotFound() {
+        Response response = given().accept(ContentType.JSON)
+                .and().pathParam("id", 500)
+                .when().get(url + "/{id}"); //api/spartans/500
+
+        System.out.println("status code = " + response.statusCode());
+        assertEquals(404, response.statusCode());
+        assertEquals(HttpStatus.SC_NOT_FOUND, response.statusCode());
+
+        assertEquals("application/json", response.contentType());
+        assertEquals(ContentType.JSON.toString() , response.contentType());
+
+        response.prettyPrint();
+
+        //asString() method returns body of response in String format
+        assertTrue(response.asString().contains("Not Found"));
+        System.out.println("asString = " + response.asString().toUpperCase());
+    }
+
 }
+
+
+
+
+
+
+
+
+
 
 
 
