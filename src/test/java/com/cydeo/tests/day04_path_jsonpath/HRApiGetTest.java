872dcb1f50360829438668bc668e09c0d3a0927d
Murodil <murodil@cybertekschool.com>
2022-08-06 12:34:23 -0400

    HR GET regions path param test

--- src/test/java/com/cydeo/tests/day04_path_jsonpath/HRApiGetTest.java
+++ src/test/java/com/cydeo/tests/day04_path_jsonpath/HRApiGetTest.java
@@ -35,14 +35,38 @@ public class HRApiGetTest {
     @Test
     public void getRegionsTest() {
         Response response = given().accept(ContentType.JSON)
-                .when().get("/regions");
+                .when().get("/regions"); //http://54.211.225.58:1000/ords/hr/regions
         response.prettyPrint();
 
         assertEquals(200, response.statusCode());
         assertEquals("application/json", response.contentType());
         assertTrue(response.asString().contains("Europe") , "Europe is not in json body");
-        //BREAK TILL 12:10 PM EST
     }
+
+    /**
+     Given accept type is json
+     And Path param "region_id" value is 1
+     When user send get request to /ords/hr/regions/{region_id}
+     Status code should be 200
+     Content type should be "application/json"
+     And body should contain "Europe"
+     */
+
+    @DisplayName("GET /regions/{region_id} path param")
+    @Test
+    public void getSingleRegionPathParamTest() {
+       Response response = given().log().all().accept(ContentType.JSON)
+                .and().pathParam("region_id", 1)
+                .when().get("/regions/{region_id}");//baseurl/regions/1
+        response.prettyPrint();
+
+        assertEquals(HttpStatus.SC_OK, response.statusCode());
+        assertEquals(ContentType.JSON.toString(), response.contentType());
+        assertTrue(response.asString().contains("Europe") , "Europe is not in json body");
+    }
+
+
+
 
 }
 
