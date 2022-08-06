02caaee54aaca6b7c2d02148f5e1c30b5f4ef4ff
Murodil <murodil@cybertekschool.com>
2022-08-06 16:44:25 -0400

    spartan path example

---
+++ src/test/java/com/cydeo/tests/day04_path_jsonpath/HRApiPathMethodTest.java
@@ -0,0 +1,7 @@
+package com.cydeo.tests.day04_path_jsonpath;
+
+import com.cydeo.utils.HRApiTestBase;
+
+public class HRApiPathMethodTest extends HRApiTestBase {
+
+}
--- src/test/java/com/cydeo/utils/HRApiTestBase.java
+++ src/test/java/com/cydeo/utils/HRApiTestBase.java
@@ -3,7 +3,7 @@ package com.cydeo.utils;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 
-public abstract class HrApiTestBase {
+public abstract class HRApiTestBase {
     @BeforeAll //@BeforeClass in junit 4
     public static void setUp() {
         RestAssured.baseURI = ConfigurationReader.getProperty("hr.api.url");
