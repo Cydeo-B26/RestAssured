9189e40029b92c7e2fd4064a9841e992ce682673
Murodil <murodil@cybertekschool.com>
2022-08-07 12:19:39 -0400

    zip code jsonpath

--- src/test/java/com/cydeo/tests/day05_path_jsonpath/ZipCodeApiJsonPathTest.java
+++ src/test/java/com/cydeo/tests/day05_path_jsonpath/ZipCodeApiJsonPathTest.java
@@ -2,6 +2,7 @@ package com.cydeo.tests.day05_path_jsonpath;
 import com.cydeo.utils.ConfigurationReader;
 import io.restassured.http.ContentType;
 
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeAll;
@@ -24,7 +25,7 @@ public class ZipCodeApiJsonPathTest {
      And  "state" is "Virginia"
      */
     @BeforeAll
-    public void setUp() {
+    public static void setUp() {
         System.out.println("Setting up base Url ... ");
         baseURI = ConfigurationReader.getProperty("zipcode.api.url");
     }
@@ -36,7 +37,21 @@ public class ZipCodeApiJsonPathTest {
                 .and().pathParam("country","us")
                 .and().pathParam("postal-code", "22102")
                 .when().get("/{country}/{postal-code}");
+       response.prettyPrint();
 
+       assertEquals(200, response.statusCode());
+
+       //assign response json payload/body to Jsonpath
+        JsonPath jsonPath = response.jsonPath();
+
+        //navigate the json and print/assert country value
+        System.out.println("country name = " + jsonPath.getString("country"));
+        assertEquals("United States" , jsonPath.getString("country"));
+
+        //navigate the json and print/assert post code value
+        System.out.println("post code = " + jsonPath.getString("'post code'"));
+        String zipCode = jsonPath.getString("'post code'");
+        assertEquals("22102", zipCode);
     }
 
 }
