36d65e1434039ced75e709265bc4225886a11627
Murodil <murodil@cybertekschool.com>
2022-08-09 20:45:14 -0400

    De-serializaton

--- src/test/java/com/cydeo/tests/day06_xmlpath_deserialization/SpartanToMapTest.java
+++ src/test/java/com/cydeo/tests/day06_xmlpath_deserialization/SpartanToMapTest.java
@@ -1,6 +1,24 @@
 package com.cydeo.tests.day06_xmlpath_deserialization;
 
-public class SpartanToMapTest {
+
+import com.cydeo.utils.SpartanTestBase;
+import io.restassured.http.ContentType;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SpartanToMapTest extends SpartanTestBase {
     /**
      Given accept type is application/json
      And Path param id = 10
@@ -13,8 +31,27 @@ public class SpartanToMapTest {
      gender >Female
      phone >3312820936
      */
+    @DisplayName("GET /spartans/{id} to MAP")
+    @Test
+    public void spartanToMapTest() {
+       Response response = given().accept(ContentType.JSON)
+                .and().pathParam("id", 10)
+                .when().get("/spartans/{id}");
+      response.prettyPrint();
+      assertEquals(HttpStatus.SC_OK, response.statusCode());
+      assertEquals("application/json", response.contentType());
+      
+      //convert json to map object. DE-SERIALIZATION
+        //Map<String, ?> spartanMap
+        Map<String, Object> spartanMap = response.body().as(Map.class);
+        System.out.println("spartanMap = " + spartanMap);
+        System.out.println("Keys = " + spartanMap.keySet());
 
-
+        assertEquals(10 , spartanMap.get("id"));
+        assertEquals("Lorenza", spartanMap.get("name"));
+        assertEquals("Female", spartanMap.get("gender"));
+        assertEquals(3312820936l, spartanMap.get("phone"));
+    }
 
 
 }
