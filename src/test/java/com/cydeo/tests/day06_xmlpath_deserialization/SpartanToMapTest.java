b10031a0add50bae4e6a6e6b43006734e7ee0638
Murodil <murodil@cybertekschool.com>
2022-08-09 20:25:28 -0400

    De-serializaton

---
+++ src/test/java/com/cydeo/tests/day06_xmlpath_deserialization/SpartanToMapTest.java
@@ -0,0 +1,19 @@
+package com.cydeo.tests.day06_xmlpath_deserialization;
+
+public class SpartanToMapTest {
+    /**
+     Given accept type is application/json
+     And Path param id = 10
+     When i send GET request to /api/spartans
+     Then status code is 200
+     And content type is json
+     And spartan data matching:
+     id > 10
+     name>Lorenza
+     gender >Female
+     phone >3312820936
+     */
+
+
+
+}
