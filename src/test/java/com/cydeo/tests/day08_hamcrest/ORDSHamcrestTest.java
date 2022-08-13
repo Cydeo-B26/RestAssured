bfbdd4a9cf59298ed6bffc22f292244e2b53da99
Murodil <murodil@cybertekschool.com>
2022-08-13 15:33:39 -0400

    Hamcrest assertions

--- src/test/java/com/cydeo/tests/day08_hamcrest/ORDSHamcrestTest.java
+++ src/test/java/com/cydeo/tests/day08_hamcrest/ORDSHamcrestTest.java
@@ -29,7 +29,7 @@ public class ORDSHamcrestTest extends HRApiTestBase {
     @DisplayName("GET /countries -> hamcrest assertions")
     @Test
     public void countriesTest() {
-        given().accept(ContentType.JSON)
+       String countryId = given().accept(ContentType.JSON)
                 .when().get("/countries")
                 .then().assertThat().statusCode(200)
                 .and().contentType(ContentType.JSON)
@@ -38,8 +38,30 @@ public class ORDSHamcrestTest extends HRApiTestBase {
                         "items.country_name", hasItems("Argentina","Australia","Belgium","Brazil","Canada"),
                         "items[0].country_id", is(equalTo("AR")) ,
                         "items[1].country_id" , is(equalTo("AU")))
-                .log().all();
+                .and().extract().body().path("items[0].country_id");
+                //.log().all();
+
+        System.out.println("countryId = " + countryId);
+
+        /**
+         *   * given accept type is json
+         *      * when I send get request to /countries/{country_id}
+         *      * Then status code is 200
+         *      * and content type is json
+         *      And country_name is Argentina, country_id is AR, region_id is 2
+         */
+        given().accept(ContentType.JSON)
+                .and().pathParam("country_id" , countryId)
+                .when().get("/countries/{country_id}")
+                .then().assertThat().statusCode(200)
+                .and().contentType(ContentType.JSON)
+                .and().body("country_name" , equalTo("Argentina"),
+                        "country_id" , equalTo("AR"),
+                        "region_id", equalTo(2));
+
     }
+
+
 
 
 }
