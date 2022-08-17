87d466a57eab3e60ba18ea2bdb536d3929abeeab
Murodil <murodil@cybertekschool.com>
2022-08-17 19:58:19 -0400

    update spartan

---
+++ src/test/java/com/cydeo/tests/day11_put_patch_request/SpartanPUTRequest.java
@@ -0,0 +1,79 @@
+package com.cydeo.tests.day11_put_patch_request;
+import com.cydeo.utils.*;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import com.cydeo.utils.SpartanRestUtils;
+import com.cydeo.utils.SpartanTestBase;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+
+import java.util.HashMap;
+import java.util.Map;
+public class SpartanPUTRequest extends SpartanTestBase {
+/**
+ Given accept type is json
+ And content type is json
+ And path param id is 15
+ And json body is
+ {
+ "gender": "Male",
+ "name": "PutRequest",
+ "phone": 1234567425
+ }
+ When i send PUT request to /spartans/{id}
+ Then status code 204
+ */
+    @DisplayName("PUT /spartans/{id}")
+    @Test
+    public void updateSpartanTest() {
+        Map<String, Object> requestMap = new HashMap<>();
+        requestMap.put("gender","Male");
+        requestMap.put("name","PutRequest");
+        requestMap.put("phone",1234567425L);
+
+        Response response = given().accept(ContentType.JSON)
+                .and().contentType(ContentType.JSON)
+                .and().pathParam("id", 15)
+                .and().body(requestMap)
+                .when().put("/spartans/{id}");
+
+        response.prettyPrint();
+        System.out.println("status code = " + response.statusCode());
+        System.out.println("status line = " + response.statusLine());
+        assertThat(response.statusCode(), is(204));
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
