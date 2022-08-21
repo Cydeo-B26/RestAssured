cc8070502d80bda9adf8cad141bd3106e7144d2a
Murodil <murodil@cybertekschool.com>
2022-08-21 14:49:05 -0400

    bookit login test

---
+++ src/test/java/com/cydeo/utils/BookItAPITestBase.java
@@ -0,0 +1,25 @@
+package com.cydeo.utils;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import static io.restassured.RestAssured.*;
+
+public abstract class BookItAPITestBase {
+
+    @BeforeAll
+    public static void setUp() {
+        baseURI = ConfigurationReader.getProperty("bookit.base.url");
+    }
+
+    public static String getAccessToken(String email, String password) {
+       String accessToken = given().accept(ContentType.JSON)
+                .and().queryParam("email", email)
+                .and().queryParam("password", password)
+                .when().get("/sign")
+                .then().assertThat().statusCode(200)
+                .and().extract().path("accessToken");
+
+       return "Bearer " + accessToken;
+    }
+
+}
