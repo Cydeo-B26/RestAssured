d3e49aeb431fabb523c4b99067b4af0314a788dd
Murodil <murodil@cybertekschool.com>
2022-08-23 20:41:17 -0400

    request spec

--- src/test/java/com/cydeo/tests/day14_specifications/SpartanSpecTest.java
+++ src/test/java/com/cydeo/tests/day14_specifications/SpartanSpecTest.java
@@ -3,6 +3,7 @@ package com.cydeo.tests.day14_specifications;
 import com.cydeo.utils.SpartanSecureTestBase;
 import com.cydeo.utils.ConfigurationReader;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
 
@@ -16,17 +17,27 @@ import static org.hamcrest.MatcherAssert.*;
 
 public class SpartanSpecTest extends SpartanSecureTestBase {
 
+    RequestSpecification requestSpec = given().accept(ContentType.JSON)
+            .and().auth().basic("admin","admin");
 
     @Test
     public void allSpartansTest() {
 
-       given().accept(ContentType.JSON)
-                .and().auth().basic("admin","admin")
-                .when().get("/spartans")
-               .then().log().all();
+//       given().accept(ContentType.JSON)
+//                .and().auth().basic("admin","admin")
 
+            given().spec(requestSpec)
+            .when().get("/spartans")
+           .then().log().all();
+    }
 
+    @Test
+    public void singleSpartanTest() {
 
+            given().spec(requestSpec)
+            .and().pathParam("id",15)
+            .when().get("/spartans/{id}")
+            .then().log().all();
     }
 
 }
--- src/test/java/com/cydeo/utils/SpartanSecureTestBase.java
+++ src/test/java/com/cydeo/utils/SpartanSecureTestBase.java
@@ -1,12 +1,22 @@
 package com.cydeo.utils;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
 
+import static io.restassured.RestAssured.given;
+
 public abstract class SpartanSecureTestBase {
+
+    protected static RequestSpecification requestSpec;
 
     @BeforeAll //@BeforeClass in junit 4
     public static void setUp() {
         RestAssured.baseURI = ConfigurationReader.getProperty("spartan.secure.api.url");
+
+        requestSpec = given().accept(ContentType.JSON)
+                .and().auth().basic("admin","admin");
+
     }
 }
