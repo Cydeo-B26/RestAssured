1463974943204ed6ac5cb0b2c092f6b64f4fdce9
Murodil <murodil@cybertekschool.com>
2022-08-23 21:32:13 -0400

    request spec

--- src/test/java/com/cydeo/utils/BookItAPITestBase.java
+++ src/test/java/com/cydeo/utils/BookItAPITestBase.java
@@ -1,6 +1,8 @@
 package com.cydeo.utils;
 
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.junit.jupiter.api.BeforeAll;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -8,9 +10,25 @@ import static org.hamcrest.MatcherAssert.*;
 
 public abstract class BookItAPITestBase {
 
-    @BeforeAll
+    protected static RequestSpecification teacherReqSpec;
+
+    protected static ResponseSpecification responseSpec;
+
+    @BeforeAll //runs before all the tests
     public static void setUp() {
         baseURI = ConfigurationReader.getProperty("bookit.base.url");
+
+        String teacherToken = getAccessToken(ConfigurationReader.getProperty("teacher_email"),
+                ConfigurationReader.getProperty("teacher_password")) ;
+
+        teacherReqSpec = given().accept(ContentType.JSON)
+                .and().header("Authorization", teacherToken)
+                .log().all();
+
+        responseSpec = expect().statusCode(200)
+                .and().contentType(ContentType.JSON)
+                .log().all();
+
     }
 
     public static String getAccessToken(String email, String password) {
