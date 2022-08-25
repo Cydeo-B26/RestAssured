6ca4a7a5a6814ac0baee0223e905a3804a69a34d
Murodil <murodil@cybertekschool.com>
2022-08-24 20:03:54 -0400

    parametrized tests

--- src/test/java/com/cydeo/tests/day15_data_driven_testing/JUnit5ValueSourceTest.java
+++ src/test/java/com/cydeo/tests/day15_data_driven_testing/JUnit5ValueSourceTest.java
@@ -1,9 +1,14 @@
 package com.cydeo.tests.day15_data_driven_testing;
 
+import com.cydeo.utils.ConfigurationReader;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static io.restassured.RestAssured.*;
+
 public class JUnit5ValueSourceTest {
 
     @ParameterizedTest
@@ -20,10 +25,20 @@ public class JUnit5ValueSourceTest {
         assertThat(name, not(blankOrNullString()));
     }
 
+    @BeforeAll
+    public static void setUp() {
+        baseURI = ConfigurationReader.getProperty("zipcode.api.url");
+    }
+
     @ParameterizedTest
     @ValueSource(ints = {22102, 22031, 22034, 11209, 15090, 15237,12345,20879,21224,33433})
     public void zipCodeTest(int zipCode) {
 
+        given().accept(ContentType.JSON)
+                .and().pathParam("postal-code", zipCode)
+                .when().get("/us/{postal-code}")
+                .then().assertThat().statusCode(200)
+                .log().all();
     }
 
 
